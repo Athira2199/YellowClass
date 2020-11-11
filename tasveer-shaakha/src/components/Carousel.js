6e70173475f42a25d4c8c38811ac4858e6178072
Athira.V.Ajit <mailathiravajit@gmail.com>
2020-11-12 01:39:05 +0530

    selected item displayed in carousel

--- tasveer-shaakha/src/components/Carousel.js
+++ tasveer-shaakha/src/components/Carousel.js
@@ -4,7 +4,7 @@ import FadeIn from 'react-fade-in';
 function Popup() {
     return(
     <FadeIn>
-        <Carousel showArrows={true} showThumbs={false}>
+        <Carousel showArrows={true} showThumbs={false} selectedItem={2}> {/* keeping current slide with arrows shown thumbs hidden */}
             <div>
                 <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
                 <p className="legend">Legend 1</p>
